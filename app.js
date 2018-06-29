89e4cb77548f89f43268a6a52ab8b41267ff8d56
alanmathew333 <mathewalan84@gmail.com>
2018-06-29 14:23:30 +0530

    Handled callback errors

--- app.js
+++ app.js
@@ -21,8 +21,14 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=india' + encodedaddr,
   json: true
 }, (error,response, body) => {
- //console.log(JSON.stringify(body, undefined, 2))
+  if (error) {
+    consol.log('Unalbe to connect to google server!')
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Address is not found!')
+  } else if(body.status === 'OK') {
+
   console.log('Address: ',body.results[0].formatted_address)
   console.log('Latitutde: ',body.results[0].geometry.location.lat)
   console.log('Longitude: ',body.results[0].geometry.location.lng)
+  }
 })
